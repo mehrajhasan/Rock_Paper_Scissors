ed3b559bb82f277663b50e2fb57631ffb164a548
mehrajhasan <114276507+mehrajhasan@users.noreply.github.com>
2023-09-22 06:00:43 -0400

    update app.js
    retrieve choice functionality + computer choice + logic for game

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req,res) => {
-    const theUrl = new URL(req.url, `http://localhost:${PORT}`);
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream('index.html').pipe(res);
-});
-
+const app = express();
 const PORT = 4001;
-server.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`);
-});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+app.listen(PORT, () =>
+    console.log(`Listening on http://localhost:${PORT}`)
+);
+
+app.post('/', (req,res) => {
+    const { info } = req.body;
+    console.log(info);
+    let user = info;
+
+    function compChoice(){
+        let choices = ['rock','paper','scissors'];
+        const result = choices[Math.floor(Math.random())*3];
+        return result;
+    }
+    
+    let gameOver;
+    let tie = "Look, we tied!";
+    let win = "Aw shucks, you win!";
+    let loss = "Ha, I win this one!";
+
+    if (user == compChoice()){
+        gameOver = tie;
+    }
+    else if(user == "rock" && compChoice() == "paper"){
+        gameOver = win;
+    }
+    else if(user == "paper" && compChoice() == "rock"){
+        gameOver = win;
+    }
+    else if(user == "scissors" && compChoice() == "paper"){
+        gameOver = win;
+    }
+    else{
+        gameOver = loss;
+    }
+    console.log(gameOver);
+})
